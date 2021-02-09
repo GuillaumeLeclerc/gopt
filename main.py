04e69322cec2aed36ddab8b8e01aa884650f58b7
Guillaume Leclerc <leclerc@mit.edu>
2021-02-09 03:03:30 -0500

    Add WinnerTakesAll Shuffler

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ from gopt import Compiler
 from gopt.problems import EuclieanTSP
 from gopt.optimizers import LocalSearch
 from gopt.runners import CPURunner
-from gopt.shufflers import IndependentShuffler
+from gopt.shufflers import IndependentShuffler, WinnerTakesAll
 
 Compiler.debug = False
 # To speed up compilation during development
 # Should be 'always' for long running jobs
-Compiler.inline = 'always'
+Compiler.inline = 'never'
 
 # root = logging.getLogger('gopt.compiler.LocalSearch')
 logging.basicConfig(format='%(levelname)s [%(name)s]:%(message)s',
@@ -20,9 +20,10 @@ logging.basicConfig(format='%(levelname)s [%(name)s]:%(message)s',
 DATA = np.random.uniform(size=(10000, 2)).astype('float32')
 NUM_CITIES = DATA.shape[0]
 
-TSP = EuclieanTSP(DATA.shape[0], DATA.shape[1], neighborhood='2-opt', init='NN')
+TSP = EuclieanTSP(DATA.shape[0], DATA.shape[1],
+                  neighborhood='2-opt', init='NN')
 Optimizer = LocalSearch(TSP)
-Shuffler = IndependentShuffler(Optimizer, 1000)
+Shuffler = WinnerTakesAll(Optimizer, 16)
 
 runner = CPURunner(Shuffler, DATA)
 result = runner.run(max_iter=1000000000, max_time='1 min')
